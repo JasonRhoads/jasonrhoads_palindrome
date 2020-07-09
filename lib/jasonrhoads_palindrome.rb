dc0dacd594988b25e1e198da3ed6b48e4db7239a
jasonrhoads <jasrhoads@gmail.com>
2020-07-09 20:36:08 +0000

    Initialize repository

---
+++ lib/jasonrhoads_palindrome.rb
@@ -0,0 +1,6 @@
+require "jasonrhoads_palindrome/version"
+
+module JasonrhoadsPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/jasonrhoads_palindrome_test.rb
@@ -0,0 +1,11 @@
+require "test_helper"
+
+class JasonrhoadsPalindromeTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::JasonrhoadsPalindrome::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
